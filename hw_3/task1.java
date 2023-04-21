9210bd250da461413c9e8d189a85b4c838e31642
Valeria Martynova <kthf175@gmail.com>
2023-04-21 20:49:11 +0500

    Added task1

---
+++ hw_3/task1.java
@@ -0,0 +1,38 @@
+package hw_3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите размер списка: ");
+        int size = scanner.nextInt(); 
+
+        //List<Integer> list = new ArrayList<>(Arrays.asList(2, 43, 22, 8, 5, 11));
+        ArrayList<Integer> numbers = new ArrayList<Integer>(size);
+        
+        System.out.println("Введите элементы списка: ");
+        
+        for (int i = 0; i < size; i++) {
+            numbers.add(scanner.nextInt());
+        }
+
+        ArrayList<Integer> resultList = removeEvenNumbers(numbers);
+       
+            System.out.println(resultList);
+        
+        
+        scanner.close();
+    }
+
+    public static ArrayList<Integer> removeEvenNumbers(ArrayList<Integer> list) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (list.get(i) % 2 == 0)
+                list.remove(i);
+        }
+        return list;
+    }
+}
